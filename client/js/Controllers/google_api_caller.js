1e84d9067080bf6444674e4401439570b5e0cf53
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-03-30 18:06:20 -0400

    added pressing 'enter' on text input

---
+++ client/js/Controllers/google_api_caller.js
@@ -0,0 +1,7 @@
+function googleCall() {
+  let twitterHandle = $("#search-field").val()
+  $.ajax(`http://localhost:3000/users/${twitterHandle}/get_tweet_text_block`).then((a) => {
+    var tweetText = a.tweet_text
+    GoogleApi.parseSentiment(tweetText)
+  })
+}
--- client/js/application.js
+++ client/js/application.js
@@ -1,11 +1,9 @@
 $(() => {
   $('[data-toggle="tooltip"]').tooltip()
-  $("#query").click(function() {
-    let twitterHandle = $("#search-field").val()
-    $.ajax(`http://localhost:3000/users/${twitterHandle}/get_tweet_text_block`).then((a) => {
-      var tweetText = a.tweet_text
-      GoogleApi.parseSentiment(tweetText)
-    })
+  $("#query").click(googleCall)
+  $("#search-field").keypress((e) => {
+    if (e.which == 13) {
+      googleCall()
+    }
   })
-
 })
