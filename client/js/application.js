cebe80512053622e25ae8abbf25b32961ef48ff1
Daniel <daniel.seehausen@gmail.com>
2017-03-30 20:00:33 -0400

    username error handled

--- client/js/application.js
+++ client/js/application.js
@@ -1,11 +1,19 @@
+function passTextToGoogleApi(tweetText) {
+  GoogleApi.parseSentiment(tweetText, twitterHandle)
+}
+
+function setNoUserFound() {
+  $("#search-field").val("User Not Found!")
+}
+
 $(() => {
   $('[data-toggle="tooltip"]').tooltip()
   $("#query").click(function() {
     let twitterHandle = $("#search-field").val() || "realDonaldTrump"
-    $.ajax(`http://localhost:3000/users/${twitterHandle}/get_tweet_text_block`).then((a) => {
-      var tweetText = a.tweet_text
-      GoogleApi.parseSentiment(tweetText, twitterHandle)
+    $.ajax({
+      url: `http://localhost:3000/users/${twitterHandle}/get_tweet_text_block`,
+      success: (data) => { passTextToGoogleApi(data.tweet_text, twitterHandle) },
+      error: () => { setNoUserFound() }
     })
   })
-
 })
