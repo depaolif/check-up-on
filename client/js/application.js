c247526a6d2383c02e6ec6588a648d9eec8adc4b
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-03-31 10:02:51 -0400

    Update application.js

--- client/js/application.js
+++ client/js/application.js
@@ -14,22 +14,9 @@ function normalizeSearchField() {
 $(() => {
   $("#search-field").click(() => { normalizeSearchField() })
   $('[data-toggle="tooltip"]').tooltip()
-
-//  $("#query").click(googleCall)
-//  $("#search-field").keypress((e) => {
-//    if (e.which == 13) {
-//    googleCall()
-//  }
-
-  $("#query").click(function() {
-    let twitterHandle = ($("#search-field").val() == "") ? "realDonaldTrump" : $("#search-field").val()
-    $.ajax({
-      url: `http://localhost:3000/users/${twitterHandle}/get_tweet_text_block`,
-      success: (data) => {
-        let userPhotos = data.user_photos
-        GoogleApi.parseSentiment(data.tweet_text, twitterHandle)
-      },
-      error: () => { setNoUserFound(twitterHandle) }
-    })
+  $("#query").click(googleCall)
+  $("#search-field").keypress((e) => {
+    if (e.which == 13) 
+      googleCall()
   })
 })
