b5cabccf375bdf9c6b9199fd84d710545ec66490
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-03-31 10:00:28 -0400

    Update google_api_caller.js

--- client/js/Controllers/google_api_caller.js
+++ client/js/Controllers/google_api_caller.js
@@ -1,7 +1,11 @@
 function googleCall() {
   let twitterHandle = $("#search-field").val()
-  $.ajax(`http://localhost:3000/users/${twitterHandle}/get_tweet_text_block`).then((a) => {
-    var tweetText = a.tweet_text
-    GoogleApi.parseSentiment(tweetText)
+  $.ajax({
+    url: `http://localhost:3000/users/${twitterHandle}/get_tweet_text_block`,
+    success: (data) => {
+      let userPhotos = data.user_photos
+      GoogleApi.parseSentiment(data.tweet_text, twitterHandle)
+    },
+    error: () => { setNoUserFound(twitterHandle) }
   })
 }
