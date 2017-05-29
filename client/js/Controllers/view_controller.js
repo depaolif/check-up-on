dc4d1efe136fb8fd266c1ca3c6149bea6e5a8ddc
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-05-29 14:02:13 -0400

    reduced twitter api call from two to one

--- client/js/Controllers/view_controller.js
+++ client/js/Controllers/view_controller.js
@@ -8,45 +8,5 @@ class ViewController {
     $("#magnitude-score").text(analyzedText.magnitude)
     $("#results").attr('style','visibility: visible')
   }
-
-  static resetView(analyzedText) {
-    $("#moodView").empty("")
-  }
-
-  static renderGreat(analyzedText) {
-    //daniel
-
-  }
-
-  static renderGood(analyzedText) {
-    //daniel
-  }
-
-  static renderOk(analyzedText) {
-    //frederico
-
-  }
-
-  static renderPoor(analyzedText) {
-    //frederico
-
-  }
-
-  static renderBad(analyzedText) {
-    //daniel
-
-  }
-
-  static renderMiserable(analyzedText) {
-    //frederico
-    currView = "miserable"
-
-
-  }
-
-  static renderMood(analyzedText) {
-    this.resetView()
-    this[`render${analyzedText.mood}`](analyzedText)
-  }
   
 }
--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ActionController::Base
 
   def get_tweet_text_block
     user_name = params[:twitter_handle]
-    text_block = combine_tweets($client.user_timeline(user_name).slice(0,10))
-    photo_url = $client.user_timeline(user_name)[0][:attrs][:user][:profile_image_url]
+    tweets = $client.user_timeline(user_name).slice(0,10)
+    text_block = combine_tweets(tweets)
+    photo_url = tweets[0][:attrs][:user][:profile_image_url]
     @json_ples = {:tweet_text => text_block, :user_photo => photo_url, :link => "https://twitter.com/#{user_name}"}.to_json
     render json: @json_ples
   end
@@ -14,7 +15,7 @@ class UsersController < ActionController::Base
   def combine_tweets(tweet_arr)
     combined_tweet_string = []
     tweet_arr.each { |t| combined_tweet_string.push(t.text) }
-    return combined_tweet_string.join(" ")
+    combined_tweet_string.join(" ")
   end
 
 end
