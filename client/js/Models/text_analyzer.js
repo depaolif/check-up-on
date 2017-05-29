973c07c9e9c8f564e8e25ab5c1b1fc3a2aaa4959
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-05-29 15:15:17 -0400

    changed a few things

--- client/js/Models/text_analyzer.js
+++ client/js/Models/text_analyzer.js
@@ -15,27 +15,27 @@ class TextAnalyzer {
     switch (true) {
       case (this.sentiment > 0.7):
         mood = 'Great'
-        snippet += " is doing great."
+        snippet += " is doing great"
         color = 'blue'
         break
       case (this.sentiment > 0.3):
         mood = 'Good'
-        snippet += " is doing pretty well."
+        snippet += " is doing pretty well"
         color = 'green'
         break
       case (this.sentiment >= 0):
         mood = 'Ok'
-        snippet += " is doing ok."
+        snippet += " is doing ok"
         color = 'yellowgreen'
         break
       case (this.sentiment > -0.3):
         mood = 'Poor'
-        snippet += " isn't doing that great."
+        snippet += " isn't doing that great"
         color = 'orange'
         break
       case (this.sentiment > -0.7):
         mood = 'Bad'
-        snippet += " is having a hard time."
+        snippet += " is having a hard time"
         color = 'sienna'
         break
       case (this.sentiment >= -1):
@@ -44,7 +44,7 @@ class TextAnalyzer {
         color = 'red'
         break
       default:
-        snippet += " is unclear how they feel."
+        snippet += " is unclear how they feel"
     }
     return [mood, snippet, color]
   }
